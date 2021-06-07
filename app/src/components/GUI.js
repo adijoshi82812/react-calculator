b9caf8541587f41d8b669a6d692cad28a2ba5350
= <=>
2021-06-07 17:44:13 +0530

    Created a boilerplate code for the project

---
+++ app/src/components/GUI.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GUI extends Component{
+    render(){
+        return(
+            <main>
+                <p>GUI will pe placed here</p>
+            </main>
+        );
+    }
+}
+
+export default GUI;
